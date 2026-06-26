3bc58bb376905a154c349b27e50db44d712b3c86
Srijan Kumar <231220062@nitdelhi.ac.in>
2026-06-26 18:53:15 +0530

    finished user work

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express" ;
 import cors from "cors" ;
-
+import cookieParser from "cookie-parser";
 const app = express() ;
 
 // basic configratons
@@ -8,6 +8,7 @@ app.use(express.json({limit: "16kb"})) ;
 app.use(express.urlencoded({extended: true, limit: "16kb"})) ;
 app.use(express.static("public")) ;
 
+app.use(cookieParser()) ;
 // cors cinfigurations
 app.use(
   cors({
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,43 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controllers.js"
+import { validate } from "../middlewares/validator.middlewares.js";
+import {verifyJWT} from "../middlewares/auth.middlewares.js" ;
+import {
+  userRegisterValidator,
+  userLoginValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
+  userChangeCurrentPasswordValidator,
+} from "../validators/index.js";
+import {
+  login,
+  registerUser,
+  logoutUser,
+  verifyEmail,
+  refreshAccessToken,
+  forgotPasswordRequest,
+  resetForgotPassword,
+  getCurrentUser,
+  changeCurrentPassword,
+  resendEmailVerification
+} from "../controllers/auth.controllers.js";
+
 
 const router = Router();
+// unsecure routes
+router.route("/register").post(userRegisterValidator(), validate,  registerUser) ;
+router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/refresh-token").post(refreshAccessToken) ;
+router
+.route("/forgot-password").post(userForgotPasswordValidator(), validate, forgotPasswordRequest) ;
+router
+.route("/reset-password/:resetToken").post(userResetForgotPasswordValidator(), validate, resetForgotPassword) ;
 
-router.route("/register").post(registerUser) ;
+// secure routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/change-password").post(verifyJWT, userChangeCurrentPasswordValidator(), validate,
+changeCurrentPassword );
+router.route("/resend-email-verification").post(verifyJWT, resendEmailVerification) ;
 
 export default router;
